b2170ce000262919685fbf923fbe0bb7ebb59596
Whitevinyl <neverest_songs@yahoo.co.uk>
2016-09-12 19:21:51 +0100

    simplified processing args

--- colorflex.js
+++ colorflex.js
@@ -2,9 +2,12 @@
 //  SETUP
 //-------------------------------------------------------------------------------------------
 
+
+//var RGBA = require('./rgba');
+
 function Color() {
     this.master = new RGBA(0,0,0,0);
-    this.highPass = new RGBA(0,0,-5,0);
+    this.highPass = new RGBA(0,0,0,0);
     this.lowPass = new RGBA(0,0,0,0);
 }
 var color = new Color();
@@ -16,23 +19,27 @@ var color = new Color();
 //-------------------------------------------------------------------------------------------
 
 Color.prototype.fill = function(ctx,col) {
-    ctx.fillStyle = this.processRGBA(col.R,col.G,col.B,col.A);
+    ctx.fillStyle = this.processRGBA(col);
 };
 
 Color.prototype.stroke = function(ctx,col) {
-    ctx.strokeStyle = this.processRGBA(col.R,col.G,col.B,col.A);
+    ctx.strokeStyle = this.processRGBA(col);
 };
 
 Color.prototype.fillRGBA = function(ctx,r,g,b,a) {
-    ctx.fillStyle = this.processRGBA(r,g,b,a);
+    ctx.fillStyle = this.processRGBA(new RGBA(r,g,b,a));
 };
 
 Color.prototype.strokeRGBA = function(ctx,r,g,b,a) {
-    ctx.strokeStyle = this.processRGBA(r,g,b,a);
+    ctx.strokeStyle = this.processRGBA(new RGBA(r,g,b,a));
 };
 
 Color.prototype.string = function(col) {
-    return this.processRGBA(col.R,col.G,col.B,col.A);
+    return this.processRGBA(col);
+};
+
+Color.prototype.stringRGBA = function(r,g,b,a) {
+    return this.processRGBA(new RGBA(r,g,b,a));
 };
 
 Color.prototype.darkerColor = function(col,darkness) {
@@ -55,13 +62,13 @@ Color.prototype.lighterColor = function(col,lightness) {
 
 
 // PASS R G B A //
-Color.prototype.processRGBA = function(r,g,b,a) {
+Color.prototype.processRGBA = function(col,object) {
 
     // master color filter //
-    var red = Math.round(r + this.master.R);
-    var green = Math.round(g + this.master.G);
-    var blue = Math.round(b + this.master.B);
-    var alpha = a + this.master.A;
+    var red = Math.round(col.R + this.master.R);
+    var green = Math.round(col.G + this.master.G);
+    var blue = Math.round(col.B + this.master.B);
+    var alpha = col.A + this.master.A;
 
     // high & low pass color filters //
     var av = ((red + green + blue) / 3);
@@ -71,16 +78,23 @@ Color.prototype.processRGBA = function(r,g,b,a) {
     green += Math.round((this.highPass.G*hp) + (this.lowPass.G*lp));
     blue += Math.round((this.highPass.B*hp) + (this.lowPass.B*lp));
 
-    // set to string //
-    return this.buildColour(red,green,blue,alpha);
-};
 
-Color.prototype.buildColour = function(red,green,blue,alpha) {
     // RANGE //
     red = this.valueInRange(red,0,255);
     green = this.valueInRange(green,0,255);
     blue = this.valueInRange(blue,0,255);
     alpha = this.valueInRange(alpha,0,1);
+
+    if (object) {
+        return new RGBA(red,green,blue,alpha);
+    } else {
+        // set to string //
+        return this.buildColour(red,green,blue,alpha);
+    }
+
+};
+
+Color.prototype.buildColour = function(red,green,blue,alpha) {
     return "rgba("+red+","+green+","+blue+","+alpha+")";
 };
 
